8db8de85a9a22dcde88fbbddd21c88bf294757f3
Peter-Ries <peterriesde@yahoo.de>
2022-09-05 18:17:06 +0200

    add iconset changes; code rearrangements

--- toggletheme.sh
+++ toggletheme.sh
@@ -18,24 +18,29 @@ config_file=$HOME/.config/toggletheme.cfg
 current_theme=$(kreadconfig5 --file $config_file --group General --key CurrentTheme)
 
 switch_wallpaper="y"
-darker_wallpaper="$HOME/Bilder/Wallpapers/ventura-dark.jpg"
-dark_wallpaper="$HOME/Bilder/Wallpapers/ventura-dark.jpg"
 light_wallpaper="$HOME/Bilder/Wallpapers/ventura.jpg"
-
-switch_shadow="y"
-darker_shadow="246,116,0" #ffa200
-dark_shadow="61,174,233" #3daee9
-light_shadow="0,0,0" #000000
+dark_wallpaper="$HOME/Bilder/Wallpapers/ventura-dark.jpg"
+darker_wallpaper="$HOME/Bilder/Wallpapers/ventura-dark.jpg"
 
 switch_colorscheme="y"
-darker_colorscheme="KritaDarkOrange"
-dark_colorscheme="BreezeClassicDarkInactiveDimmed"
 light_colorscheme="BreezeClassic"
+dark_colorscheme="BreezeClassicDarkInactiveDimmed"
+darker_colorscheme="KritaDarkOrange"
+
+switch_shadow="y"
+light_shadow="0,0,0" #000000
+dark_shadow="61,174,233" #3daee9
+darker_shadow="246,116,0" #ffa200
+
+switch_icons="y"
+light_icons="breeze"
+dark_icons="breeze-dark"
+darker_icons="breeze-dark"
 
 switch_konsole="y"
-darker_konsole="Breeze"
-dark_konsole="Breeze"
 light_konsole="BlackOnWhite"
+dark_konsole="Breeze"
+darker_konsole="Breeze"
 
 switch_vscode="y"
 
@@ -66,9 +71,28 @@ switchShadow() {
         #
         # set ShadowColor (R,G,B) in $HOME/.config/breezerc
         kwriteconfig5 --file breezerc --group Common --key ShadowColor $1
+    fi
+}
+
+switchIcons() {
+    if [[ $switch_icons == "y" ]] ; then
+        /usr/lib/plasma-changeicons $1
+    fi
+}
+
+switchKonsole () {
+    if [[ $switch_konsole == "y" ]] ; then
         #
-        # reload config to apply
-        qdbus org.kde.KWin /KWin org.kde.KWin.reconfigure
+        # define Breeze.profile and BlackOnWhite.profile profiles first for konsole!
+        # set default profile for new instances
+        kwriteconfig5 --file konsolerc --group "Desktop Entry" --key "DefaultProfile" $1".profile"
+        #
+        # apply to all running instances
+        IFS=" "
+        pids=$(pidof konsole)
+        for pid in $pids; do
+            $(qdbus org.kde.konsole-$pid /Sessions/1 org.kde.konsole.Session.setProfile $1)
+        done
     fi
 }
 
@@ -90,21 +114,6 @@ switchVSCode () {
     fi
 }
 
-switchKonsole () {
-    if [[ $switch_konsole == "y" ]] ; then
-        #
-        # define Breeze.profile and BlackOnWhite.profile profiles first for konsole!
-        # set default profile for new instances
-        kwriteconfig5 --file konsolerc --group "Desktop Entry" --key "DefaultProfile" $1".profile"
-        #
-        # apply to all running instances
-        IFS=" "
-        pids=$(pidof konsole)
-        for pid in $pids; do
-            $(qdbus org.kde.konsole-$pid /Sessions/1 org.kde.konsole.Session.setProfile $1)
-        done
-    fi
-}
 
 
 
@@ -141,23 +150,26 @@ case $new_theme in
     light)
         new_wallpaper=$light_wallpaper
         new_colorscheme=$light_colorscheme
-        new_vscode=$new_theme
-        new_konsole=$light_konsole
         new_shadow=$light_shadow
+        new_icons=$light_icons
+        new_konsole=$light_konsole
+        new_vscode=$new_theme
         ;;
     dark)
         new_wallpaper=$dark_wallpaper
         new_colorscheme=$dark_colorscheme
-        new_vscode=$new_theme
-        new_konsole=$dark_konsole
         new_shadow=$dark_shadow
+        new_icons=$dark_icons
+        new_konsole=$dark_konsole
+        new_vscode=$new_theme
         ;;
     darker)
         new_wallpaper=$darker_wallpaper
         new_colorscheme=$darker_colorscheme
-        new_vscode=$new_theme
-        new_konsole=$darker_konsole
         new_shadow=$darker_shadow
+        new_icons=$darker_icons
+        new_konsole=$darker_konsole
+        new_vscode=$new_theme
         ;;
     *)
         echo Only light, dark, darker allowed as parameter.
@@ -168,8 +180,13 @@ esac
 switchWallpaper $new_wallpaper
 switchColorscheme $new_colorscheme
 switchShadow $new_shadow
-switchVSCode $new_vscode
+switchIcons $new_icons
 switchKonsole $new_konsole
+switchVSCode $new_vscode
+
+#
+# reload config to apply changes
+qdbus org.kde.KWin /KWin org.kde.KWin.reconfigure
 
 #
 # write config
