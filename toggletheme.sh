fbca95b3cf3ee31713a35926cf5cf0486b17e602
Peter-Ries <peterriesde@yahoo.de>
2022-09-09 19:10:45 +0200

    Refactor variable names to uppercase.

--- toggletheme.sh
+++ toggletheme.sh
@@ -14,35 +14,40 @@
 # #########################################################
 
 # read config
-config_file=$HOME/.config/toggletheme.cfg
-current_theme=$(kreadconfig5 --file $config_file --group General --key CurrentTheme)
+CONFIG_FILE=$HOME/.config/toggletheme.cfg
+CURRENT_THEME=$(kreadconfig5 --file $CONFIG_FILE --group General --key CurrentTheme)
 
-switch_wallpaper="y"
-light_wallpaper="$HOME/Bilder/Wallpapers/ventura.jpg"
-dark_wallpaper="$HOME/Bilder/Wallpapers/ventura-dark.jpg"
-darker_wallpaper="$HOME/Bilder/Wallpapers/ventura-dark.jpg"
+BLACK="0,0,0" #000000
+BLUE="61,174,233" #3daee9
+ORANGE="246,116,0" #ffa200
 
-switch_colorscheme="y"
-light_colorscheme="BreezeClassic"
-dark_colorscheme="BreezeClassicDarkInactiveDimmed"
-darker_colorscheme="KritaDarkOrange"
+SWITCH_WALLPAPER="y"
+LIGHT_WALLPAPER="$HOME/Bilder/Wallpapers/ventura.jpg"
+DARK_WALLPAPER="$HOME/Bilder/Wallpapers/ventura-dark.jpg"
+DARKER_WALLPAPER="$HOME/Bilder/Wallpapers/ventura-dark.jpg"
 
-switch_shadow="y"
-light_shadow="0,0,0" #000000
-dark_shadow="61,174,233" #3daee9
-darker_shadow="246,116,0" #ffa200
+SWITCH_COLORSCHEME="y"
+LIGHT_COLORSCHEME="BreezeClassic"
+DARK_COLORSCHEME="BreezeClassicDarkInactiveDimmed"
+DARKER_COLORSCHEME="KritaDarkOrange"
 
-switch_icons="y"
-light_icons="breeze"
-dark_icons="breeze-dark"
-darker_icons="breeze-dark"
+SWITCH_SHADOW="y"
+LIGHT_SHADOW=$BLACK
+DARK_SHADOW=$BLUE
+DARKER_SHADOW=$ORANGE
 
-switch_konsole="y"
-light_konsole="BlackOnWhite"
-dark_konsole="Breeze"
-darker_konsole="Breeze"
+SWITCH_ICONS="y"
+LIGHT_ICONS="breeze"
+DARK_ICONS="breeze-dark"
+DARKER_ICONS="breeze-dark"
 
-switch_vscode="y"
+SWITCH_KONSOLE="y"
+# create corresponding konsole profiles. put name without extension here.
+LIGHT_KONSOLE="BlackOnWhite"
+DARK_KONSOLE="Breeze"
+DARKER_KONSOLE="Breeze"
+
+SWITCH_VSCODE="y"
 
 
 # #########################################################
@@ -50,7 +55,7 @@ switch_vscode="y"
 # #########################################################
 
 switchWallpaper() {
-    if [[ $switch_wallpaper == "y" ]] ; then
+    if [[ $SWITCH_WALLPAPER == "y" ]] ; then
         #
         # switch wallpaper
         plasma-apply-wallpaperimage $1
@@ -61,13 +66,13 @@ switchWallpaper() {
 }
 
 switchColorscheme() {
-    if [[ $switch_colorscheme == "y" ]] ; then
+    if [[ $SWITCH_COLORSCHEME == "y" ]] ; then
         plasma-apply-colorscheme $1
     fi
 }
 
 switchShadow() {
-    if [[ $switch_shadow == "y" ]] ; then
+    if [[ $SWITCH_SHADOW == "y" ]] ; then
         #
         # set ShadowColor (R,G,B) in $HOME/.config/breezerc
         kwriteconfig5 --file breezerc --group Common --key ShadowColor $1
@@ -75,13 +80,13 @@ switchShadow() {
 }
 
 switchIcons() {
-    if [[ $switch_icons == "y" ]] ; then
+    if [[ $SWITCH_ICONS == "y" ]] ; then
         /usr/lib/plasma-changeicons $1
     fi
 }
 
 switchKonsole () {
-    if [[ $switch_konsole == "y" ]] ; then
+    if [[ $SWITCH_KONSOLE == "y" ]] ; then
         #
         # define Breeze.profile and BlackOnWhite.profile profiles first for konsole!
         # set default profile for new instances
@@ -89,15 +94,15 @@ switchKonsole () {
         #
         # apply to all running instances
         IFS=" "
-        pids=$(pidof konsole)
-        for pid in $pids; do
-            $(qdbus org.kde.konsole-$pid /Sessions/1 org.kde.konsole.Session.setProfile $1)
+        PIDS=$(pidof konsole)
+        for PID in $PIDS; do
+            $(qdbus org.kde.konsole-$PID /Sessions/1 org.kde.konsole.Session.setProfile $1)
         done
     fi
 }
 
 switchVSCode () {
-    if [[ $switch_vscode == "y" ]] ; then
+    if [[ $SWITCH_VSCODE == "y" ]] ; then
         #
         # hard replacement of colorTheme string in settings.json
         case $1 in
@@ -124,12 +129,12 @@ switchVSCode () {
 if [[ $1 == "" ]] ; then
     #
     # no parameter - only toggle light/dark
-    case $current_theme in
+    case $CURRENT_THEME in
         light)
-            new_theme="dark"
+            NEW_THEME="dark"
             ;;
         *)
-            new_theme="light"
+            NEW_THEME="light"
             ;;
     esac
 else
@@ -137,7 +142,7 @@ else
     # parameter -> evaluate
     case $1 in
         light | dark | darker )
-            new_theme=$1
+            NEW_THEME=$1
             ;;
         *)
             echo Only light, dark, darker allowed as parameter.
@@ -146,31 +151,30 @@ else
     esac
 fi
 
-case $new_theme in
+case $NEW_THEME in
     light)
-        new_wallpaper=$light_wallpaper
-        new_colorscheme=$light_colorscheme
-        new_shadow=$light_shadow
-        new_icons=$light_icons
-        new_konsole=$light_konsole
-        new_vscode=$new_theme
+        NEW_WALLPAPER=$LIGHT_WALLPAPER
+        NEW_COLORSCHEME=$LIGHT_COLORSCHEME
+        NEW_SHADOW=$LIGHT_SHADOW
+        NEW_ICONS=$LIGHT_ICONS
+        NEW_KONSOLE=$LIGHT_KONSOLE
+        NEW_VSCODE=$NEW_THEME
         ;;
     dark)
-        new_wallpaper=$dark_wallpaper
-        new_colorscheme=$dark_colorscheme
-        # new_shadow=$dark_shadow
-        new_shadow=$darker_shadow
-        new_icons=$dark_icons
-        new_konsole=$dark_konsole
-        new_vscode=$new_theme
+        NEW_WALLPAPER=$DARK_WALLPAPER
+        NEW_COLORSCHEME=$DARK_COLORSCHEME
+        NEW_SHADOW=$DARK_SHADOW
+        NEW_ICONS=$DARK_ICONS
+        NEW_KONSOLE=$DARK_KONSOLE
+        NEW_VSCODE=$NEW_THEME
         ;;
     darker)
-        new_wallpaper=$darker_wallpaper
-        new_colorscheme=$darker_colorscheme
-        new_shadow=$darker_shadow
-        new_icons=$darker_icons
-        new_konsole=$darker_konsole
-        new_vscode=$new_theme
+        NEW_WALLPAPER=$DARKER_WALLPAPER
+        NEW_COLORSCHEME=$DARKER_COLORSCHEME
+        NEW_SHADOW=$DARKER_SHADOW
+        NEW_ICONS=$DARKER_ICONS
+        NEW_KONSOLE=$DARKER_KONSOLE
+        NEW_VSCODE=$NEW_THEME
         ;;
     *)
         echo Only light, dark, darker allowed as parameter.
@@ -178,12 +182,12 @@ case $new_theme in
         ;;
 esac
 
-switchWallpaper $new_wallpaper
-switchColorscheme $new_colorscheme
-switchShadow $new_shadow
-switchIcons $new_icons
-switchKonsole $new_konsole
-switchVSCode $new_vscode
+switchWallpaper $NEW_WALLPAPER
+switchColorscheme $NEW_COLORSCHEME
+switchShadow $NEW_SHADOW
+switchIcons $NEW_ICONS
+switchKonsole $NEW_KONSOLE
+switchVSCode $NEW_VSCODE
 
 #
 # reload config to apply changes
@@ -191,7 +195,7 @@ qdbus org.kde.KWin /KWin org.kde.KWin.reconfigure
 
 #
 # write config
-kwriteconfig5 --file $config_file --group General --key CurrentTheme $new_theme
+kwriteconfig5 --file $CONFIG_FILE --group General --key CurrentTheme $NEW_THEME
 
 # #########################################################
 # END OF SCRIPT
